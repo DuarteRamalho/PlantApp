660255d008f568da08be8201ab97d79126825e61
Duarte Baptista Ramalho <1221806@isep.ipp.pt>
2024-10-06 18:57:02 +0200

    Camera access Implementation

--- app/src/main/java/com/example/plantapp/MainActivity.kt
+++ app/src/main/java/com/example/plantapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.plantapp
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.plantapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val CAMERA_PREMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
 
     lateinit var binding: ActivityMainBinding
 
@@ -23,7 +37,20 @@ class MainActivity : AppCompatActivity() {
 
         binding.AddFlowersBtn.setOnClickListener{
 
-            replaceFragment(SecondFragment())
+            if(ContextCompat.checkSelfPermission(
+                this,
+                        Manifest.permission.CAMERA
+                    ) == PackageManager.PERMISSION_GRANTED
+                ){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PREMISSION_CODE
+                )
+            }
 
         }
     }
@@ -35,5 +62,37 @@ class MainActivity : AppCompatActivity() {
 
         fragmentTransaction.replace(R.id.fragmentContainer,fragment)
         fragmentTransaction.commit()
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == CAMERA_REQUEST_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                Toast.makeText(this,
+                    "Oops you just denied the permission for camera. " +
+                    "Don't worry you can allow it in the settings. ",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == CAMERA_REQUEST_CODE){
+                val thumbNail: Bitmap = data!!.extras!!.get("data") as Bitmap
+                binding.ivImage.setImageBitmap(thumbNail)
+            }
+        }
     }
 }
